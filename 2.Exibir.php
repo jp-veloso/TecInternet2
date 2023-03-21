e131e15679a1e5f113a97630466a48361ea6fd45
neexxus <joaopedro.veloso@outlook.com>
2023-03-20 21:28:54 -0300

    [FIX] Code

--- 2.Exibir.php
+++ 2.Exibir.php
@@ -1,39 +1,49 @@
-<?php 
-  $texto = '';
+<?php
 
-  if (isset($_GET['texto'])) {
-    $texto = $_GET['texto'];
-  }
+$visualizar = '';
+$fotoanimal = '';
+
+if(isset($_GET['visualizar'])){
+    $visualizar = $_GET['visualizar'];
+}
+
+switch($visualizar){
+    case 'cachorros':
+        $fotoanimal = "https://img.freepik.com/fotos-gratis/lindo-retrato-de-cachorro_23-2149218452.jpg";
+        break;
+
+    case 'gatos':
+        $fotoanimal = 'https://ogimg.infoglobo.com.br/in/24649523-8a0-224/FT1086A/88249115.jpg';
+        break;
+
+    case 'tartarugas':
+        $fotoanimal = 'https://media.istockphoto.com/id/93385233/pt/foto/tartaruga-da-birm%C3%A2nia.jpg?b=1&s=612x612&w=0&k=20&c=rsIKnxmPuKvaOuIgCc8YmqNmpT8PIFlPMGkJcah-BZY=';
+        break;
+}
+
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Parametro</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fotos de animais</title>
 </head>
-
-<style>
-  body {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-  }
-  input {
-    display: block;
-    margin-bottom: 24px;
-  }
-</style>
-
 <body>
-    <form>
-      <input type="text" name="texto" placeholder="Digite algo aqui" required >
-      <input type="submit">
+Selecione uma das páginas que deseja:
+<form method="GET">
+    <select name="visualizar">
+        <option value="cachorros">Cachorros</option>
+        <option value="gatos">Gatos</option>
+        <option value="tartarugas">Tartarugas</option>
+    </select>
+    <br><br>
+    <input type="submit" value="visualizar"><br><br>
 
-      <p>Observe na URL, você digitou <?=$texto?> e apareceu o mesmo em cima</p>
-    </form>
+    Olha que fofura:<br>
+    <img src="<?=$fotoanimal?>">
+</form>
 </body>
 </html>
